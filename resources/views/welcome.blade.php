1c1ade7889c05a0832e74d4cb027104036a6f3f6
Ryan <ryangroenewold@hotmail.com>
2021-04-28 22:14:33 +0200

    Added animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,48 +13,126 @@
         
         <!-- Loading in font awesome -->
         <script src="https://kit.fontawesome.com/73201f4522.js" crossorigin="anonymous"></script>
+
+        <!-- Jquery loading -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <script>
+            $(document).ready(function() {
+ 
+                setTimeout(function(){
+                    $('body').addClass('loaded');
+                    $('#bgvid').trigger('play');
+                }, 1500);
+
+            });
+        </script>
     </head>
     <!-- <img src="{{ URL('/images/640x360.png') }}" alt="" title=""> -->
     <body>
-        <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-            <!-- <div class="container"> -->
-                <a class="navbar-brand" href="#"><image src="{{URL('/images/MaikoRossProdLogoWit.png')}}"></a>
-            <!-- </div> -->
-        </nav>
+        <!-- Beginning of our preloader -->
+        <div id="loader-wrapper">
+            <div id="loader"></div>
 
-        <div class="video-background">
-            <div class="video-wrap">
-                <div id="video">
-                    <video id="bgvid" autoplay loop muted>
-                        <source src="{{ URL('/video/Site Preview V2 (720p).mp4')}}" type="video/mp4">
-                    </video>
-                </div>
-            </div>
+            <div class="loader-section section-left"></div>
+            <div class="loader-section section-right"></div>
         </div>
 
-        <div class="caption text-center">
-            <div class="section__box">
-                <a href="#0" class="r-link ai-element ai-element_type1 ai-element1 homePageCaption">
-                    <span class="ai-element__label ">Over ons</span>
-                </a>
+        <div class="content">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+                <!-- <div class="container"> -->
+                    <a class="navbar-brand" href="#"><image src="{{URL('/images/MaikoRossProdLogoWit.png')}}"></a>
+                <!-- </div> -->
+            </nav>
+
+            <div class="video-background">
+                <div class="video-wrap">
+                    <div id="video">
+                        <video id="bgvid" preload="metadata" loop muted >
+                            <source src="{{ URL('/video/Site Preview V2 (720p).mp4')}}" type="video/mp4">
+                        </video>
+                    </div>
+                </div>
+            </div>
+
+            <div class="caption text-center">
+                <div class="section__box">
+                    <a href="#0" class="r-link ai-element ai-element_type1 ai-element1 homePageCaption">
+                        <span class="ai-element__label ">Over ons</span>
+                    </a>
+                </div>
             </div>
             <div class="arrow bounce">
                 <i class="fa fa-angle-down fa-5x" aria-hidden="true"></i>
             </div>
-        </div>
 
-        <div id="course" class="row justify-content-center">
-            <div class="col-md-8 text-center">
-                <h1> Test header plus </h1>
-                <p class="lead"> NDWUIAHDUWAHDUIWAHDUWAIH hWUDIHA hwdui hdwuaiA WHui udhqwih uiqd hu </p>
+            <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div>
+            <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
+            </div> <div id="klanten" class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1> Test header plus </h1>
+                    
+                </div>
             </div>
         </div>
-        
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
     <script>
         $(window).scroll(function(){
             $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+            $(".caption").css("opacity", 1 - $(window).scrollTop() / 250); 
         //250 is fade pixels
         }); 
   </script>    
