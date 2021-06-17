489b34a3d7738a0d6209ce3b8ea2244214080aa6
Ryan <ryangroenewold@hotmail.com>
2021-06-17 16:43:36 +0200

    Test

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -56,7 +56,7 @@ class ContactFormController extends Controller
         $c->message = $request->message;
         $c->save();
         
-        Mail::to("info@maikoross.nl")
+        Mail::to("ryan@maikoross.nl")
                 ->send(new ContactFormMail ($c));
     }
 
